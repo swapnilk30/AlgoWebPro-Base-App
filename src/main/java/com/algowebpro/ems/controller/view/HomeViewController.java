bcb8260c82387f270f8596ba2d6a34a72397e779
swapnilk30 <sk9137587676@gmail.com>
2025-11-11 21:42:35 +0530

    install locally bootstrap 5.3.8

--- src/main/java/com/algowebpro/ems/controller/view/HomeViewController.java
+++ src/main/java/com/algowebpro/ems/controller/view/HomeViewController.java
@@ -1,6 +1,7 @@
 package com.algowebpro.ems.controller.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,10 @@ public class HomeViewController {
     @RequestMapping("/home")
     public String getHome(){
         return "home";
+    }
+    
+    @GetMapping("/")
+    public String home() {
+        return "index"; // loads templates/index.html
     }
 }
